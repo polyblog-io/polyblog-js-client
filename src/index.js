e6bceb576dd0144c518ee8b0a18e2d5ff7eeaa8d
Giorgio Zamparelli <giorgio.zamparelli@gmail.com>
2021-12-16 11:23:43 +0100

    feat: allow index import like import { getArticles } from '@polyblog/polyblog-js-client'

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-function log(msg){
-  console.log(msg);
-};
-module.exports = log;
+import getArticleById from './getArticleById'
+import getArticleBySlugAndByLocale from './getArticleBySlugAndByLocale'
+import getArticles from './getArticles'
+
+module.exports = {
+  getArticleById,
+  getArticleBySlugAndByLocale,
+  getArticles,
+}
