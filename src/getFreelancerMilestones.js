2fc3c497cee7f16ba9f2fd7558c69425416fcf02
Giorgio Zamparelli <giorgio.zamparelli@gmail.com>
2022-01-22 13:54:31 +0100

    chore: remove freelancer.com api endpoints

--- src/getFreelancerMilestones.js
+++
@@ -1,9 +0,0 @@
-import get from './get'
-
-export default function getFreelancerMilestones(projectId) {
-  let url = `freelancercom/projects/${projectId}/milestones/`
-
-  return get({ url }).catch(error => {
-    throw new Error(`Couldn't get freelancer.com milestones \n${error}`)
-  })
-}
--- src/getFreelancerProjects.js
+++
@@ -1,9 +0,0 @@
-import get from './get'
-
-export default function getFreelancerProjects() {
-  let url = 'freelancercom/projects'
-
-  return get({ url }).catch(error => {
-    throw new Error(`Couldn't get freelancer.com projects\n${error}`)
-  })
-}
